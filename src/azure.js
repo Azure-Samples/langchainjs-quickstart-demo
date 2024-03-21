c520ef00b77f5233ba03583c464cf583333b03e5
sinedied <noda@free.fr>
2024-03-21 10:04:37 +0100

    chore: restore the base versions

---
+++ src/azure.js
@@ -0,0 +1,78 @@
+import "dotenv/config";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+import { AzureChatOpenAI } from "@langchain/azure-openai";
+import { AzureOpenAIEmbeddings } from "@langchain/azure-openai";
+import {
+  AzureAISearchVectorStore,
+  AzureAISearchQueryType,
+} from "@langchain/community/vectorstores/azure_aisearch";
+
+const YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=FZhbJZEgKQ4";
+const QUESTION = "What are the news about GPT-4 models?";
+
+// Load documents ------------------------------------------------------------
+
+console.log("Loading documents...");
+
+const loader = YoutubeLoader.createFromUrl(YOUTUBE_VIDEO_URL, {
+  language: "en",
+  addVideoInfo: true,
+});
+const rawDocuments = await loader.load();
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 2000,
+  chunkOverlap: 400,
+});
+const documents = await splitter.splitDocuments(rawDocuments);
+
+// Init models and DB --------------------------------------------------------
+
+console.log("Initializing models and DB...");
+
+const embeddings = new AzureOpenAIEmbeddings();
+const model = new AzureChatOpenAI();
+const vectorStore = new AzureAISearchVectorStore(embeddings, {
+  search: { type: AzureAISearchQueryType.SimilarityHybrid },
+});
+
+// Search if documents already exist for the source video
+const videoId = YOUTUBE_VIDEO_URL.split("v=")[1];
+const indexedDocuments = await vectorStore.similaritySearch("*", 1, {
+  filterExpression: `metadata/source eq '${videoId}'`,
+});
+
+if (indexedDocuments.length === 0) {
+  console.log("Embedding documents...");
+  await vectorStore.addDocuments(documents);
+}
+
+// Run the chain -------------------------------------------------------------
+
+console.log("Running the chain...");
+
+const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
+  ["system", "Answer the user's question using only the sources below:\n\n{context}"],
+  ["human", "{input}"],
+]);
+const combineDocsChain = await createStuffDocumentsChain({
+  prompt: questionAnsweringPrompt,
+  llm: model,
+});
+const chain = await createRetrievalChain({
+  retriever: vectorStore.asRetriever(),
+  combineDocsChain,
+});
+const stream = await chain.stream({ input: QUESTION });
+
+// Print the result ----------------------------------------------------------
+
+console.log(`Answer for the question "${QUESTION}":\n`);
+for await (const chunk of stream) {
+  process.stdout.write(chunk.answer ?? "");
+}
+console.log();
---
+++ src/local.js
@@ -0,0 +1,65 @@
+import "dotenv/config";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { YoutubeLoader } from "langchain/document_loaders/web/youtube";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+import { ChatOllama } from "@langchain/community/chat_models/ollama";
+import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
+import { FaissStore } from "@langchain/community/vectorstores/faiss";
+
+const YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=FZhbJZEgKQ4";
+const QUESTION = "What are the news about GPT-4 models?";
+
+// Load documents ------------------------------------------------------------
+
+console.log("Loading documents...");
+
+const loader = YoutubeLoader.createFromUrl(YOUTUBE_VIDEO_URL, {
+  language: "en",
+  addVideoInfo: true,
+});
+const rawDocuments = await loader.load();
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 2000,
+  chunkOverlap: 400,
+});
+const documents = await splitter.splitDocuments(rawDocuments);
+
+// Init models and DB --------------------------------------------------------
+
+console.log("Initializing models and DB...");
+
+const embeddings = new OllamaEmbeddings({ model: "all-minilm:l6-v2" });
+const model = new ChatOllama({ model: "llama2" });
+const vectorStore = new FaissStore(embeddings, {});
+
+console.log("Embedding documents...");
+vectorStore.addDocuments(documents);
+
+// Run the chain -------------------------------------------------------------
+
+console.log("Running the chain...");
+
+const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
+  ["system", "Answer the user's question using only the sources below:\n\n{context}"],
+  ["human", "{input}"],
+]);
+const combineDocsChain = await createStuffDocumentsChain({
+  prompt: questionAnsweringPrompt,
+  llm: model,
+});
+const chain = await createRetrievalChain({
+  retriever: vectorStore.asRetriever(),
+  combineDocsChain,
+});
+const stream = await chain.stream({ input: QUESTION });
+
+// Print the result ----------------------------------------------------------
+
+console.log(`Answer for the question "${QUESTION}":\n`);
+for await (const chunk of stream) {
+  process.stdout.write(chunk.answer ?? "");
+}
+console.log();
